1270350f953626d2eddc5fd5ff47e20e15b2150d
Robert <robertvantimmeren@gmail.com>
2022-03-13 21:56:02 +0100

    Read data now also reads other datasets.

--- Data/read_data.py
+++ Data/read_data.py
@@ -1,15 +1,21 @@
 import os
 import re
+import sys
 
 
-def read_data(location):
+def read_data(location: str) -> list:
+    """Scans through the specified data folder and reads in
+    all the data that is stored in the appropriate files.
+
+    :param location: location of the folder
+    :return: all combined data
+    """
     data = []
     temp = []
     with os.scandir(location) as directory:
         for entry in directory:
-            if entry.name.endswith('.conll') and entry.is_file():
-                #print(entry.path, entry.file)
-                with open(entry.path) as file:
+            if entry.name.endswith('.conll') or entry.name.endswith('features') and entry.is_file():
+                with open(entry.path, encoding='utf8') as file:
                     in_file = file.readlines()
                     in_file.append('\n')
                     for line in in_file:
@@ -21,74 +27,90 @@ def read_data(location):
     return data
 
 
-def remove_placeholders(parc_dev):
+def extract_tokens_labels(all_data: list) -> list:
+    '''Extracts all tokens and corresponding labels per token.
+
+    :param all_data: data from all files
+    :return: sentences of tuples of a token and its label(s)
+    '''
     # Find the tokens: (?:[A-Za-z-[0-9])+
     # Find blanco tokens: (?:B-|I-)(?:CUE|CONTENT|SOURCE)
-    clean_parc_dev = []
-    for sentence in parc_dev:
+    tokens_labels = []
+    for sentence in all_data:
         temp = []
         for token in sentence:
-            label = token[-1].replace('_', '').split()
-            temp.append((token[8], label))
-        clean_parc_dev.append(temp)
+            if len(token) == 12 or len(token) == 20:
+                label = token[-1].replace('_', '').split()
+                temp.append((token[8], label))
+            elif len(token) == 11:
+                label = token[-1].replace('_', '').split()
+                temp.append((token[3], label))
+        tokens_labels.append(temp)
+    return tokens_labels
 
-    clean_parc_dev_3 = []
+
+def structure_data(tokens_labels, argv):
+    '''Removed nested ARs and further prepare the data for working with it.
+
+    :param tokens_labels: tokens and corresponding labels
+    :return: structured data
+    '''
+    clean_data = []
     nested_regex = re.compile(r'[A-Za-z\-]+-NE-[0-9]+')
     label_regex = re.compile(r'(?:B-|I-)(?:CUE|CONTENT|SOURCE)')
 
-    # Improved version
-    for sentence in clean_parc_dev:
-        temp = []
-        for label_tokens in sentence:
-            tokens = label_tokens[1]
-            filtered_token = [i for i in tokens if not nested_regex.match(i)]
-            if filtered_token != []:
-                match = re.match(label_regex, filtered_token[0])
-                temp.append((label_tokens[0], match[0]))
-            else:
-                temp.append((label_tokens[0], 'O'))
-        clean_parc_dev_3.append(temp)
-
-
-    clean_parc_dev_2 = []
-    regex = re.compile(r'(?:B-|I-)(?:CUE|CONTENT|SOURCE)')
-    for i in clean_parc_dev:
-        first_token = ''
-        temp = []
-        for j in i:
-            # set first found token
-            if first_token == '':
-                if j[1] != []:
-                    if len(first_token) > 1:
-                        first_token = j[1][0]
-                    else:
-                        first_token = j[1]
-                        #print(first_token)
-            # append only equals
-            if j[1] != [] and j[1][0][-2:] == first_token[0][-2:]:
-                #print(j[1][0])
-                match = re.match(regex, j[1][0])
-                if match is not None:
-                    temp.append((j[0], match[0]))
+    if 'PARC3.0' in argv or 'POLNEAR' in argv:
+        for sentence in tokens_labels:
+            temp = []
+            for label_tokens in sentence:
+                tokens = label_tokens[1]
+                filtered_token = [i for i in tokens if not nested_regex.match(i)]
+                if filtered_token != []:
+                    match = re.match(label_regex, filtered_token[0])
+                    temp.append((label_tokens[0], match[0]))
                 else:
-                    temp.append((j[0], 'O'))
-            else:
-                temp.append((j[0], 'O'))
-        clean_parc_dev_2.append(temp)
+                    temp.append((label_tokens[0], 'O'))
+            clean_data.append(temp)
 
-    return clean_parc_dev_3
+    elif 'VaccinationCorpus' in argv:
+        for sentence in tokens_labels:
+            first_token = ''
+            temp = []
+            for i in sentence:
+                if first_token == '':
+                    if i[1] != []:
+                        if len(first_token) > 1:
+                            first_token = i[1][0]
+                            print(first_token)
+                        else:
+                            first_token = i[1]
+                            print(first_token)
+                if i[1] != [] and i[1][0][-2:] == first_token[0][-2:]:
+                    match = re.match(label_regex, i[1][0])
+                    if match is not None:
+                        temp.append((i[0], match[0]))
+                    else:
+                        temp.append((i[0], 'O'))
+                else:
+                    temp.append((i[0], 'O'))
+            clean_data.append(temp)
+
+    return clean_data
 
 
-def main(arg):
-    parc_dev = read_data(arg)
-    #print(parc_dev[0])
-    clean_parc_dev = remove_placeholders(parc_dev)
-    for i in clean_parc_dev:
+def main(argv: list) -> list:
+    # parc loc: "Data/PARC3.0/PARC_tab_format/dev"
+    # polnear loc: "Data/POLNEAR_enriched/dev"
+    # vaccination loc: "Data/VaccinationCorpus"
+
+    all_data = read_data(argv)
+    tokens_labels = extract_tokens_labels(all_data)
+    clean_data = structure_data(tokens_labels, argv)
+    for i in clean_data:
         for j in i:
             print(j)
-    #print(clean_parc_dev[0])
-    return clean_parc_dev
+    return clean_data
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
