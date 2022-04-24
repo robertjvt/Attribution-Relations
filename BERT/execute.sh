7b8135a5811f478702c077ac4706210ebb0ac4b3
Robert <robertvantimmeren@gmail.com>
2022-04-24 22:41:22 +0200

    Changes to execute bash for Peregrine to use correct modules and virtual environment

--- BERT/execute.sh
+++ BERT/execute.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=this_is_a_test
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=r.j.van.timmeren@student.rug.nl
+#SBATCH --job-name=BERT_ARs
 #SBATCH --output=job-%j.log
 #SBATCH --time=01:59:00
 #SBATCH --partition=gpushort
 #SBATCH --gres=gpu:1
 
+module purge
+module load Python/3.8.6-GCCcore-10.2.0
+
+source /data/s4535553/.envs/first_env/bin/activate
+
 python3 train_bert.py
+
+deactivate
